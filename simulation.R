0fdeae99676d4c75b07549c02cc25ef12a1ed50c
Camila <cnbarragani@unal.edu.co>
2026-01-12 18:55:27 +0100

    Change in Bayes factor
    Use bain to compute the Bayes facto

--- simulation.R
+++ simulation.R
@@ -10,17 +10,19 @@ library(tidyverse)
 
 # Functions
 source("ssd_null.R")
-source("ssd_inf.R")
+#source("ssd_inf.R")
 source("helpers_simulation.R")
 
 # common factors --------------
 var_u0 <- c(0, 0.25, 0.5, 1)
-eff_size_beta <- c(0, 0.5, 1, 1.5)
+eff_size_beta <- c(0.5, 1, 1.5)
 eff_size_prob <- 1 / (1 + exp(-eff_size_beta))
 BF_thres <- c(1, 3, 5)
 b_fract <- 3
 eta <- 0.8
 ndatasets <- 5000
+Max <- 1000
+batch_size <- 500
 
 ########## Hypothesis Set 1: Equality vs. informative #############################
 
@@ -34,45 +36,65 @@ dir.create(results_folder)
 n1 <- c(5, 10, 40)
 design_matrixN2 <- expand.grid(var_u0, eff_size_prob, BF_thres, eta, n1, fixed <- "n1")
 nrow_designN2 <- nrow(design_matrixN2)
-colnames(design_matrixN2) <- c("var_u0", "eff_size", "BF_threshold", "eta", "n1", "fixed")
+colnames(design_matrixN2) <- c("var_u0", "p_int", "BF_threshold", "eta", "n1", "fixed")
 design_matrixN2 <- mutate(design_matrixN2, seed = as.integer(sample(2^32 /
                                                                         2, n())))
 
+run_null_wrapper <- function(Row) {
+    run_null(
+        Row = Row,
+        design_matrix = design_matrixN2,
+        ndatasets = ndatasets,
+        Max = Max,
+        batch_size = batch_size,
+        results_folder = results_folder,
+        b = b_fract
+    )
+}
+
 ### Running the simulation ---
 clusters <- makeForkCluster(detectCores() * 0.75)
-clusters <- makeCluster(detectCores() * 0.75) #For Windows
+#clusters <- makeCluster(detectCores() * 0.75) #For Windows
 clusterEvalQ(clusters, {
     library(tidyverse)
+    library(lme4)
+    library(dplyr)
+    library(bain)
 })
-clusterExport(
-    clusters,
-    c(
-        "design_matrixN2",
-        "ndatasets",
-        "b",
-        "Max",
-        "batch_size",
-        "results_folder",
-        "binary_search_eq",
-        "collect_results",
-        "collect_times",
-        "extract_res",
-        "filter_underpowered",
-        "final_binary_search",
-        "fit_glmer",
-        "get_variance",
-        "marker_func",
-        "print_results",
-        "reached_condition",
-        "round2",
-        "run_null",
-        "run_simulation_inf",
-        "SSD_crt_null_binary",
-        "varcov"
-    )
+# clusterExport(
+#   clusters,
+#   c(
+#     "design_matrixN2",
+#     "ndatasets",
+#     "b_fract",
+#     "Max",
+#     "batch_size",
+#     "results_folder",
+#     "binary_search_eq",
+#     "collect_results",
+#     "collect_times",
+#     "extract_res_bain",
+#     "filter_underpowered",
+#     "binary_search_eq",
+#     "fit_glmer",
+#     "get_variance",
+#     "marker_func",
+#     "print_results",
+#     "reached_condition",
+#     "round2",
+#     "run_null",
+#     "SSD_crt_null_binary",
+#     "varcov",
+#     "run_null_wrapper"
+#   ), envir = environment()
+# )
+
+output <- parallel::parLapply(
+    cl = clusters,
+    X = 4:nrow_designN2,
+    fun = run_null_wrapper
 )
 
-output <- pbapply(1:nrow_designN2, run_null, cl = clusters)
 stopCluster(clusters)
 
 ### Collect results -----
